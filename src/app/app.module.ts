79c762cc6de4bf2d056c60e3d303e10538d17ae4
YerUlan <51732861+turgan6ek@users.noreply.github.com>
2020-09-09 09:38:30 +0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Week2Component } from './week2/week2.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchboxComponent } from './searchbox/searchbox.component';
 import { ListComponent } from './list/list.component';
@@ -11,7 +10,6 @@ import { ListComponent } from './list/list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    Week2Component,
     HeaderComponent,
     SearchboxComponent,
     ListComponent
