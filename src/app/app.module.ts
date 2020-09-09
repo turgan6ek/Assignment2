8e8707a10530491247bceb3fa73a32b8db99d974
YerUlan <51732861+turgan6ek@users.noreply.github.com>
2020-09-09 09:34:00 +0600

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Week2Component } from './week2/week2.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchboxComponent } from './searchbox/searchbox.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Week2Component,
+    HeaderComponent,
+    SearchboxComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
